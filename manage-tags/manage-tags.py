e517a9077d4f892109f826006f674e2759449e60
Compsky <adammarkgray@gmail.com>
2019-05-29 16:35:18 +0100

    Fixes

--- manage-tags/manage-tags.py
+++ manage-tags/manage-tags.py
@@ -5,49 +5,75 @@ from   itertools import chain
 import mysql.connector
 from   PyQt5.QtCore import QMimeData, QModelIndex, Qt
 from   PyQt5.QtWidgets import QApplication, QMainWindow, QTreeView, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QToolButton, QHeaderView
-from   PyQt5.QtGui import QStandardItem, QStandardItemModel
+from   PyQt5.QtGui import QStandardItem, QStandardItemModel, QDragMoveEvent
+from   re import sub
 import sys
 
 
-#n_uncommited_edits:int = 0
+tagid2name:dict = {}
+tagid2directoccurances:dict = {}
+tagid2occurances:dict = {0: 0}
 
 
 def cursor_execute(s:str):
     print(s)
-    cursor.execute(s, multi=False) # WARNING: If multi is set to False, it will automatically commit the last stored commit
+    cursor.execute(s, multi=False) # WARNING: If multi is set to False, it will automatically commit the last stored commit. If True, it will not commit anything.
     cnx.commit()
-    #global n_uncommited_edits
-    #n_uncommited_edits += 1
-    #win.commit_btn.setText(f"Commit {n_uncommited_edits} edits")
 
-'''
-def cursor_commit(dunno:bool):
-    cnx.commit()
-    global n_uncommited_edits
-    n_uncommited_edits = 0
-    win.commit_btn.setText("Nothing to commit")
-'''
 
 
 class PrimaryItem(QStandardItem):
     def delete_self(self):
-        # Does not call TagTreeModel::removeRows
-        s:str = f"DELETE FROM tag2parent WHERE parent_id={self.parent().text() or '0'} AND tag_id={self.text()}"
+        # The default does not call TagTreeModel::removeRows
+        parent = self.parent()
+        t:str = "0"
+        if (parent is not None):
+            t = parent.text()
+        s:str = f"DELETE FROM tag2parent WHERE parent_id={t} AND tag_id={self.text()}"
         cursor_execute(s)
-        self.parent().removeRow(self.row())
+        if (parent is None):
+            self.model.removeRow(self.row())
+        else:
+            parent.removeRow(self.row())
 
 
 class TagTreeModel(QStandardItemModel):
-    def dropMimeData(self, data:QMimeData, action, row:int, col:int, parent:QModelIndex):
-        # Dropped items by default can be dropped onto any column; this is nonsensical for our purposes
-        # NOTE: Probably platform dependant. See available formats with data.formats()
-        if (not super().dropMimeData(data, action, row, 0, parent)):
+    tagid2entry:dict = {}
+    tag2parent:dict = {}
+    dragging = False
+    
+    def dropMimeData(self, data:QMimeData, action, row:int, col:int, dst_parent:QModelIndex):
+        self.dragging = False
+        
+        b:bytes = sub(
+            b"^"
+            b"[^0-9]*"
+            b"((?:[0-9]\0)+)" # Tag ID
+            b"[^0-9]*" # Separators and tag name
+            b"[0-9]+" # Number of direct occurances
+            b"[^0-9]"
+            b"(?:.|\n)*"
+            b"$",
+            b"\\1",
+            data.data("application/x-qabstractitemmodeldatalist").data())
+        # NOTE: MIME is probably platform dependant. See available formats with data.formats()
+        
+        tag_id:int = int(sub(b"\0", b"", b))
+        
+        current_parent_id:int = self.tag2parent[tag_id]
+        
+        parent_tag_id:int = int(dst_parent.data() or 0)
+        
+        if (parent_tag_id == current_parent_id):
             return False
-        parent_tag_id:str = parent.data() or "0"
-        tag_id:str = self.index(self.rowCount(parent)-1, 0, parent).data()
+        
+        if (not super().dropMimeData(data, action, row, 0, dst_parent)):
+            return False
+        
         s:str = f"INSERT IGNORE INTO tag2parent (parent_id, tag_id) VALUES ({parent_tag_id}, {tag_id})"
         cursor_execute(s)
         return True
+    
     def removeRows(self, row:int, count:int, parent:QModelIndex):
         tag_id:str = self.index(row, 0, parent).data()
         if (not super().removeRows(row, count, parent)):
@@ -56,7 +82,32 @@ class TagTreeModel(QStandardItemModel):
         s:str = f"DELETE FROM tag2parent WHERE parent_id={parent_tag_id} AND tag_id={tag_id}"
         cursor_execute(s)
         return True
+    
+    def moveRows(self, src_parent:QModelIndex, row:int, count:int, dst_parent:QModelIndex, dst_child:int):
+        print(src_parent.text(), row, count, dst_parent.text(), dst_child)
     # NOTE: moveRows is not called when moving a row between two parents
+    
+    def item_changed(self, item:QStandardItem):
+        if (self.dragging):
+            # We only want to update the name if the item change is due to editing the name, not moving
+            return
+        tag_id_:str = item.index().siblingAtColumn(0).data()
+        if (tag_id_ is None):
+            return
+        tag_id:int = int(tag_id_)
+        txt:str = item.text()
+        if (txt == ""):
+            return
+        if (txt == tagid2name[tag_id]):
+            return
+        try:
+            # Avoid setting names as integers, just in case
+            int(txt)
+            return
+        except ValueError:
+            pass
+        tagid2name[tag_id] = txt
+        cursor_execute(f"UPDATE tag SET name=\"{txt}\" WHERE id={tag_id_}")
 
 
 class TagTreeView(QTreeView):
@@ -68,21 +119,22 @@ class TagTreeView(QTreeView):
         
         if (use_tagtreemodel):
             self.model = TagTreeModel(0, 4, parent)
+            self.model.itemChanged.connect(self.model.item_changed)
         else:
             self.model = QStandardItemModel(0, 4, parent)
         
         self.setModel(self.model)
         
-        self.init_headers()
+        self.init_headers() 
         
     def init_headers(self):
         self.model.setHeaderData(self.ID,        Qt.Horizontal, "ID")
         self.model.setHeaderData(self.NAME,      Qt.Horizontal, "Name")
-        self.model.setHeaderData(self.COUNT,     Qt.Horizontal, "Direct Occurances") # i.e. does not include the count of tags inheriting from it
-        self.model.setHeaderData(self.DLT_BTN,   Qt.Horizontal, "Delete")
+        self.model.setHeaderData(self.COUNT,     Qt.Horizontal, "Occurances") # Either direct - i.e. does not include the count of tags inheriting from it - or inherited
+        self.model.setHeaderData(self.DLT_BTN,   Qt.Horizontal, "Unchild")
         
     def place_tags(self, root:int, expand:bool):
-        self.tagid2entry:dict = {root: self.model}
+        self.model.tagid2entry = {root: self.model}
         queue:list = []
         res:list = [x for x in cursor]
         n_results:int = len(res)
@@ -115,7 +167,7 @@ class TagTreeView(QTreeView):
             entry__delete.setDropEnabled(False)
             
             try:
-                self.tagid2entry[parent_id].appendRow([entry__id, entry__name, entry__count, entry__delete])
+                self.model.tagid2entry[parent_id].appendRow([entry__id, entry__name, entry__count, entry__delete])
             except KeyError:
                 queue.append((parent_id, tag_id, name, count))
                 continue
@@ -127,7 +179,15 @@ class TagTreeView(QTreeView):
             if (expand):
                 self.expand(entry__id.index())
             
-            self.tagid2entry[tag_id] = entry__id
+            self.model.tag2parent[tag_id] = parent_id
+            self.model.tagid2entry[tag_id] = entry__id
+            tagid2name[tag_id] = name
+            tagid2directoccurances[tag_id] = tagid2occurances[tag_id] = count
+            tagid2occurances[parent_id] += count
+    
+    def dragMoveEvent(self, e:QDragMoveEvent):
+        self.model.is_dragging = True
+        super().dragMoveEvent(e)
 
 
 class TagChildTreeView(TagTreeView):
@@ -155,7 +215,8 @@ class TagParentTreeView(TagTreeView):
         self.setDragDropOverwriteMode(False)
     
     def set_root(self):
-        self.tagid2entry = {} # Not unnecessary
+        self.model.tagid2entry = {} # Not unnecessary
+        self.model.tag2parent = {}
         self.init_headers()
         
         for _ in range(self.model.rowCount()):
@@ -163,7 +224,7 @@ class TagParentTreeView(TagTreeView):
         
         tag_id:str = self.tag_child_tree_view.selectionModel().currentIndex().siblingAtColumn(0).data()
         cursor.execute(f"CALL ancestor_tags_id_rooted_from_id(\"tmp_tag_parents\", {tag_id})", multi=False)
-        cursor.execute("SELECT node, parent, name, 0 FROM tmp_tag_parents JOIN tag ON id=parent WHERE node")
+        cursor.execute("SELECT node, parent, name, -1 FROM tmp_tag_parents JOIN tag ON id=parent WHERE node")
         
         self.place_tags(int(tag_id), False)
 
