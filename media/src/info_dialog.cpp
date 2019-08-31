dc1664190cf4f39659c1f88dabc0325b655acabe
NotCompsky <adammarkgray@gmail.com>
2019-08-31 10:59:52 +0100

    Tidy: const where possible.

--- media/src/info_dialog.cpp
+++ media/src/info_dialog.cpp
@@ -30,13 +30,13 @@ InfoDialog::InfoDialog(const uint64_t file_id,  const qint64 file_sz,  QWidget* 
 	QLocale locale = this->locale();
 	l->addWidget(new QLabel(QString("File size: ") + locale.formattedDataSize(file_sz)));
 	
-	char* _score_str;
+	const char* _score_str;
 	compsky::mysql::query(_mysql::obj, RES1, BUF, "SELECT score FROM file WHERE id=", this->file_id);
 	while(compsky::mysql::assign_next_row(RES1, &ROW1, &_score_str)){
 		l->addWidget(new QLabel(QString("Score: %1").arg(_score_str)));
 	}
 	
-	char* _file_path;
+	const char* _file_path;
 	compsky::mysql::query(_mysql::obj, RES1, BUF, "SELECT name FROM file WHERE id=", this->file_id);
 	while(compsky::mysql::assign_next_row(RES1, &ROW1, &_file_path)){
 		l->addWidget(new QLineEdit(_file_path));
@@ -44,7 +44,7 @@ InfoDialog::InfoDialog(const uint64_t file_id,  const qint64 file_sz,  QWidget* 
 	
 	compsky::mysql::query(_mysql::obj, RES1, BUF, "SELECT t.id, t.name FROM file2tag f2t, tag t WHERE t.id=f2t.tag_id AND f2t.file_id=", this->file_id);
 	uint64_t _tagid;
-	char* _tagname;
+	const char* _tagname;
 	while(compsky::mysql::assign_next_row(RES1, &ROW1, &_tagid, &_tagname)){
 		QHBoxLayout* hbox = new QHBoxLayout;
 		
