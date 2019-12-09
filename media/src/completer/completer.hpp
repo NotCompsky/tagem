a67c0e3a54ba06da0924d355ce724992d195f404
NotCompsky <adammarkgray@gmail.com>
2019-12-09 21:28:14 +0000

    Tidy:  Remove innacurate copyright information

--- media/src/completer/completer.hpp
+++ media/src/completer/completer.hpp
@@ -1,10 +1,3 @@
-/*
- * CC BY-SA 4.0
- * 
- * Original code is copyright Marek R (https://stackoverflow.com/users/1387438/marek-r) and was viewed from https://stackoverflow.com/questions/31465850/a-qlineedit-qcombobox-search-that-ignores-diacritics
- */
-
-
 #pragma once
 #include <QCompleter>
 #include "simplify_str.hpp"
