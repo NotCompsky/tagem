ac4303c36eb99bd485095dea4ec73d0a2de8e955
NotCompsky <adammarkgray@gmail.com>
2020-01-01 20:17:11 +0000

    Tidy,Fix:  Flatten SQL query.            This fixes a bug that only occurs if one of the tag|tag2parent tables is broken somehow (i.e. this bug would not arise through use of this software, only if the database was manually edited).

--- media/src/tag-manager/tagchildtreeview.cpp
+++ media/src/tag-manager/tagchildtreeview.cpp
@@ -16,6 +16,14 @@ TagChildTreeView::TagChildTreeView(QWidget* parent) : TagTreeView(true, parent) 
     this->setDragDropMode(this->InternalMove);
     this->setDragDropOverwriteMode(false);
     
-    compsky::mysql::query_buffer(_mysql::obj, RES1,  "SELECT parent_id, B.id, B.c, B.name FROM tag2parent t2p LEFT JOIN (SELECT id, name, COUNT(A.tag_id) as c FROM tag LEFT JOIN(SELECT tag_id FROM file2tag) A ON A.tag_id = id GROUP BY id, name) B ON B.id = t2p.tag_id ORDER BY t2p.parent_id ASC");
+	compsky::mysql::query_buffer(
+		_mysql::obj,
+		RES1,
+		"SELECT t2p.parent_id, t.id, COUNT(f2t.file_id), t.name "
+		"FROM tag2parent t2p "
+		"JOIN tag t ON t.id=t2p.tag_id "
+		"JOIN file2tag f2t ON f2t.tag_id=t.id "
+		"GROUP BY t2p.parent_id, t.id "
+	);
     this->place_tags(0);
 };
