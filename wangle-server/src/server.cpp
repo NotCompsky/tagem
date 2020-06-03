185e9086ece31957def4e48a52ecc1442cd5cea6
NotCompsky <adammarkgray@gmail.com>
2020-06-03 02:51:07 +0100

    Feature		Display stuff on the new tag (profile) page. 		Including tag thumbnail and tag cover image, with limits raised to 200 bytes.

--- wangle-server/src/server.cpp
+++ wangle-server/src/server.cpp
@@ -406,6 +406,20 @@ namespace _r {
 	};
 	
 	
+	void asciifiis(const flag::Dict& _flag,  char*& itr,  const uint64_t* n,  const char* const* str1,  const char* const* str2,  const char* const* str3){
+		compsky::asciify::asciify(
+			itr,
+			'"', *n, '"', ':', '[',
+				'"', _f::esc, '"', *str1, '"', ',',
+				'"', _f::esc, '"', *str2, '"', ',',
+				'"', _f::esc, '"', *str3, '"',
+			']', ','
+		);
+	}
+	size_t strlens(const flag::Dict& _flag,  const uint64_t*,  const char* const* str1,  const char* const* str2,  const char* const* str3){
+		return std::char_traits<char>::length("\"1234567890123456789\":[\"\",\"\",\"\"],") + 2*strlen(*str1) + 2*strlen(*str2) + 2*strlen(*str3);
+	}
+	
 	void asciifiis(const flag::Dict& _flag,  char*& itr,  const uint64_t* n,  const char* const* str1,  const char* const* str2){
 		compsky::asciify::asciify(
 			itr,
@@ -1594,9 +1608,10 @@ int main(int argc,  char** argv){
 	uint64_t id2;
 	const char* name;
 	const char* str2;
+	const char* str3;
 	constexpr _r::flag::Dict dict;
 	constexpr _r::flag::Arr  arr;
-	_r::init_json(dict, "SELECT id, name, thumbnail FROM tag", _r::tags_json, &id, &name, &str2);
+	_r::init_json(dict, "SELECT id, name, thumbnail, cover FROM tag", _r::tags_json, &id, &name, &str2, &str3);
 	_r::init_json(arr,  "SELECT tag_id, parent_id FROM tag2parent", _r::tag2parent_json, &id, &id2);
 	_r::init_json(dict, "SELECT id, name FROM protocol", _r::protocols_json, &id, &name);
 	_r::init_json(dict, "SELECT id, name, device FROM dir", _r::dirs_json, &id, &name, &id2);
