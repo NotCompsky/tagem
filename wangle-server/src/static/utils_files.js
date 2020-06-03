d63b88ce4d06379d3ebe166f501bac4aad9d61ed
NotCompsky <adammarkgray@gmail.com>
2020-06-03 13:09:38 +0100

    Tidy		Use map instead of for loop. 		This will be reverted for performance reasons.

--- wangle-server/src/static/utils_files.js
+++ wangle-server/src/static/utils_files.js
@@ -1,18 +1,9 @@
 "function populate_f_table(url, selector){"
 	"get_json(url, function(data){"
-		"var s = \"\";"
-		"for (var ls of data){"
-			"s += \"<div class='tr' data-id='\" + ls[1] + \"'>\";"
-				"s += '<div class=\"td\"><img class=\"thumb\" src=\"' + ls[0] + '\"></img></div>';"
-				//"s += \"<td><a href='/d#\" + ls[1] + \"'>\" + ls[2] + \"</a></td>\";" // Dir  ID and name
-				"s += \"<div class='td fname'><a href='/f#\" + ls[1] + \"'>\" + ls[2] + \"</a></div>\";" // File ID and name
-				"s += \"<div class='td'>\" + ls[3] + \"</div>\";"
-				
-			"s += \"</div>\";"
-		"}"
-		"$(selector).html(s);"
+		"document.querySelector(selector).innerHTML = data.map(tpl => '<div class=\"tr\" data-id=\"'+tpl[1]+'\"><div class=\"td\"><img class=\"thumb\" src=\"'+tpl[0]+'\"/></div><div class=\"td fname\"><a href=\"/f#'+tpl[1]+'\">'+tpl[2]+'</a></div><div class=\"td\">'+tpl[3]+'</div>').join();"
 		"column_id2name(\"/a/t.json\", selector, '/t#', 2);"
 	"});"
+	
 "}"
 "function tag_files(file_ids, tagselect){"
 	"$.post({"
