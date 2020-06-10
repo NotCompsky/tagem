66cafb5d2ee3c180bd3785871b937235651dd48a
NotCompsky <adammarkgray@gmail.com>
2020-06-10 10:45:16 +0100

    Documentation		Clarify

--- wangle-server/src/qry.hpp
+++ wangle-server/src/qry.hpp
@@ -31,7 +31,7 @@ successness::ReturnType parse_into(char* itr,  const char* qry);
 	"			f (file)\n" \
 	"			d (dir)\n" \
 	"			D (dir - matching against only the basename)\n" \
-	"			x (external database)\n" \
+	"			x (for file only - file appears in the named external database)\n" \
 	"	value [MIN]-[MAX] [[LIST_OF_NAMES]]\n" \
 	"		Entries have a variable named one of the listed names, which has a value in the specified range\n" \
 	"		Each name is separated by a double quote (\")\n" \
