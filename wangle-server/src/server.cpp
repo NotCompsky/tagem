fe5e72e1baf6296f885f4971281f2a14aea7ef11
NotCompsky <adammarkgray@gmail.com>
2020-06-16 10:42:42 +0100

    Placeholder		Add function to get cookie value. Placeholder		Add login endpoint.

--- wangle-server/src/server.cpp
+++ wangle-server/src/server.cpp
@@ -283,6 +283,109 @@ const char* skip_to_post_data(const char* s){
 }
 
 constexpr
+const char* get_cookies(const char* headers){
+	// Returns a pointer to the character immediately BEFORE the cookies list (i.e. the space character in "Cookie: "). This is a silly little micro-optimistaion.
+	while(*(++headers) != 0){ // NOTE: headers is guaranteed to be more than 0 characters long, as we have already guaranteed that it starts with the file id
+		if (*headers != '\n')
+			continue;
+		bool is_cookie_header = false;
+		switch(*(++headers)){
+			case 'C':
+				switch(*(++headers)){
+					case 'o':
+						switch(*(++headers)){
+							case 'o':
+								switch(*(++headers)){
+									case 'k':
+										switch(*(++headers)){
+											case 'i':
+												switch(*(++headers)){
+													case 'e':
+														switch(*(++headers)){
+															case ':':
+																switch(*(++headers)){
+																	case ' ':
+																		is_cookie_header = true;
+																		break;
+																}
+																break;
+														}
+														break;
+												}
+												break;
+										}
+										break;
+								}
+								break;
+						}
+						break;
+				}
+				break;
+		}
+		if (unlikely(*headers == 0))
+			break;
+		if (likely(not is_cookie_header))
+			continue;
+		
+		return headers;
+	}
+	return nullptr;
+}
+
+struct NullableStringView {
+	const char* data;
+	size_t sz;
+	
+	constexpr
+	NullableStringView()
+	: data(nullptr)
+	, sz(0)
+	{}
+};
+
+constexpr
+NullableStringView get_cookie(const char* s,  const char* const cookie_name){
+	// NOTE: cookie_name should include the equals sign
+	const char* cookies = get_cookies(s);
+	NullableStringView desired_cookie;
+	if (cookies == nullptr){
+		return desired_cookie;
+	}
+	while(*(++cookies) != 0){
+		const char* cookie_name_itr = cookie_name;
+		while(*cookies == *cookie_name_itr){
+			++cookie_name_itr;
+			++cookies;
+			if(*cookie_name_itr == 0)
+				desired_cookie.data = cookies;
+		}
+		
+		// Skip to next cookie name
+		while((*cookies != ';') and (*cookies != 0))
+			++cookies;
+		desired_cookie.sz = (uintptr_t)cookies - (uintptr_t)desired_cookie.data;
+		switch(*cookies){
+			case 0:
+				// Probably invalid end of headers
+			case '\n':
+				// No more cookies
+				return desired_cookie;
+			default: // ';'
+				switch(*(++cookies)){
+					case ' ':
+						// Good
+						break;
+					default:
+						return desired_cookie;
+				}
+		}
+		if (desired_cookie.data != nullptr)
+			break;
+	}
+	return desired_cookie;
+}
+
+constexpr
 GetRangeHeaderResult get_range(const char* headers,  size_t& from,  size_t& to){
 	while(*(++headers) != 0){ // NOTE: headers is guaranteed to be more than 0 characters long, as we have already guaranteed that it starts with the file id
 		if (*headers != '\n')
@@ -2562,6 +2665,29 @@ class RTaggerHandler : public wangle::HandlerAdapter<const char*,  const std::st
 								switch(*(s++)){
 									case '/':
 										return this->parse_qry(s);
+								}
+								break;
+							case 'l':
+								switch(*(s++)){
+									case 'o':
+										switch(*(s++)){
+											case 'g':
+												switch(*(s++)){
+													case 'i':
+														switch(*(s++)){
+															case 'n':
+																switch(*(s++)){
+																	case ' ':
+																		// /login
+																		break;
+																}
+																break;
+														}
+														break;
+												}
+												break;
+										}
+										break;
 								}
 								break;
 						}
