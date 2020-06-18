a441397a564e90a456b4f428e12b37ffe4407cd8
NotCompsky <adammarkgray@gmail.com>
2020-06-18 12:59:32 +0100

    Feature		Automatically overwrite directory in dirselect.

--- wangle-server/src/static/utils_select2.js
+++ wangle-server/src/static/utils_select2.js
@@ -1,4 +1,4 @@
 "function guess_parenty_thing_from_name(var_name, s){"
-	"return Object.entries(window[var_name]).map(([id, tpl]) => [tpl[0], id]).filter(x => s.startsWith(x[0])).sort().reverse()[0][1];"
+	"return Object.entries(window[var_name]).map(([id, tpl]) => [id, tpl[0]]).filter(x => s.startsWith(x[1])).sort().reverse()[0];"
 	// Use something like $('#dirselect').val(KEY).trigger('change') to update select2 selection box
 "}"
