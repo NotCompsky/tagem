c83c18c0cefc8c6228a92e681af09f589c5a376d
NotCompsky <adammarkgray@gmail.com>
2020-06-21 14:02:15 +0100

    Fix

--- wangle-server/src/static/utils_select2.js
+++ wangle-server/src/static/utils_select2.js
@@ -1,4 +1,7 @@
 "function guess_parenty_thing_from_name(var_name, s){"
-	"return Object.entries(window[var_name]).map(([id, tpl]) => [id, tpl[0]]).filter(x => s.startsWith(x[1])).sort(function(a,b){return b[1].length-a[1].length;})[0];"
+	"if(var_name==='P')"
+		"return Object.entries(window[var_name]).map(([id, tpl]) => [id, tpl]).filter(x => s.startsWith(x[1])).sort(function(a,b){return b[1].length-a[1].length;})[0];"
+	"else "
+		"return Object.entries(window[var_name]).map(([id, tpl]) => [id, tpl[0]]).filter(x => s.startsWith(x[1])).sort(function(a,b){return b[1].length-a[1].length;})[0];"
 	// Use something like $('#dirselect').val(KEY).trigger('change') to update select2 selection box
 "}"
