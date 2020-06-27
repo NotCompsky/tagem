8684080a1be6231978e9b5ecc305df29a591e397
NotCompsky <adammarkgray@gmail.com>
2020-06-27 21:11:51 +0100

    Feature		Add ability to login and logout.

--- wangle-server/src/static/utils.js
+++ wangle-server/src/static/utils.js
@@ -61,3 +61,15 @@
 		"init_selects(var_name);"
 	"});"
 "}"
+
+"function login(){"
+	"const uname = prompt(\"Username\");"
+	"set_cookie(\"username\", uname, 3600*24);" // Super 100% secure login with inbuilt blockchain neural networks
+	"document.getElementById('username').innerText = uname;"
+	"refetch_all_jsons();"
+"}"
+"function logout(){"
+	"unset_cookie(\"username\");"
+	"document.getElementById('username').innerText = \"GUEST\";"
+	"refetch_all_jsons();"
+"}"
