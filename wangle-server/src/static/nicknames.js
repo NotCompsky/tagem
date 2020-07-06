79fc8a36b9b99e9d7c9e6d23e0b64a47ca7a5aab
NotCompsky <adammarkgray@gmail.com>
2020-07-06 02:30:03 +0100

    Fix		Avoid duplicate entries in recent history list.

--- wangle-server/src/static/nicknames.js
+++ wangle-server/src/static/nicknames.js
@@ -10,9 +10,13 @@ function $$$add_window_location_hash_to_history(){
 	if(($$$window_location.hash==="")||($$$document.title===""))
 		return;
 	const x = $$$document_getElementById('recent-pages');
+	for(node of x.childNodes){
+		if(node.innerText===$$$document.title)
+			return;
+	}
 	if(x.childNodes.length===10)
 		x.removeChild(x.childNodes[9]);
-	x.innerHTML = "<a onclick='$$$load_page_from_a_hash_string(\""+$$$window_location.hash+"\")'>" + $$$document.title + "</a>" + x.innerHTML;
+	x.innerHTML = "<a onclick='$$$load_page_from_a_hash_string(\""+$$$window_location.hash+"\")'>" + $$$document.title + "</a>" + x.innerHTML; // WARNING: Not escaped
 }
 function $$$set_window_location_hash(s){
 	$$$add_window_location_hash_to_history();
