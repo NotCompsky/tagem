bfafe718fb618725412c959a09a2f627b6aedd04
NotCompsky <adammarkgray@gmail.com>
2020-07-14 14:56:19 +0100

    Tidy		Remove the workaround, as it is no longer necessary (using the better workaround of not sending large files).

--- wangle-server/src/CStringCodec.h
+++ wangle-server/src/CStringCodec.h
@@ -48,27 +48,7 @@ class CStringCodec : public Handler<std::unique_ptr<folly::IOBuf>, const std::st
   }
 
   folly::Future<folly::Unit> write(Context* ctx,  const std::string_view msg) override {
-		constexpr size_t min_chunk_sz = 100;
-		size_t chunk_sz = 10 * 1024;
-		/*
-		 * The chunk size needs to become smaller for larger files.
-		 * Sending files in one go seems to be fine for files below a megabyte.
-		 * But for sending a 14MiB file, even a (constant) chunk_sz as low as 1000 regularly fails to deliver the file without truncation.
-		 * I have no idea why this is the case.
-		 * TODO: Fix this
-		 */
-		size_t sz = msg.size();
-		auto data = msg.data();
-		while(sz > chunk_sz){
-			auto buf = folly::IOBuf::copyBuffer(data, chunk_sz);
-			data += chunk_sz;
-			sz -= chunk_sz;
-			chunk_sz /= 2;
-			if (chunk_sz < min_chunk_sz)
-				chunk_sz = min_chunk_sz;
-			ctx->fireWrite(std::move(buf));
-		}
-		auto buf = folly::IOBuf::copyBuffer(data, sz);
+		auto buf = folly::IOBuf::copyBuffer(msg.data(), msg.size());
 		return ctx->fireWrite(std::move(buf));
   }
 };
