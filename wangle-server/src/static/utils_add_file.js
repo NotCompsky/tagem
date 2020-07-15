b240cb4920a364eee73bc41ab01c1cb64ab658d6
NotCompsky <adammarkgray@gmail.com>
2020-07-15 21:22:19 +0100

    Fix		Initialise checkbox to unchecked state.

--- wangle-server/src/static/utils_add_file.js
+++ wangle-server/src/static/utils_add_file.js
@@ -1,4 +1,5 @@
 function $$$add_files_dialog(){
+	$$$document_getElementById('add-f-backup-toggle').checked = false;
 	$$$hide_all_except(['tagselect-files-container','add-f-backup-toggle-container','add-f-dialog']);
 	$$$hide('add-f-backup-url');
 	// TODO: Toggle switch to allow selecting dir ID for backup
