22ab9d4aa8faf3c1e3e892befe59820c7e2fcec3
NotCompsky <adammarkgray@gmail.com>
2020-07-15 21:22:58 +0100

    Update		Use new field names.

--- procedures.sql
+++ procedures.sql
@@ -5,14 +5,14 @@ delimiter $$
 CREATE PROCEDURE gen_tag2parent_tree()
 BEGIN
     TRUNCATE TABLE tag2parent_tree;
-    INSERT INTO tag2parent_tree (tag, parent, depth) SELECT id, id, 0 FROM _tag;
+    INSERT INTO tag2parent_tree (id, parent, depth) SELECT id, id, 0 FROM _tag;
     
     DROP TABLE IF EXISTS _tmp;
     CREATE TEMPORARY TABLE _tmp LIKE tag2parent_tree;
     REPEAT
         TRUNCATE TABLE _tmp;
-        INSERT INTO _tmp SELECT t2pt.tag, t2p.parent, t2pt.depth+1 FROM tag2parent_tree t2pt JOIN tag2parent t2p ON t2p.tag=t2pt.parent ON DUPLICATE KEY UPDATE depth=LEAST(_tmp.depth, t2pt.depth+1);
-        INSERT INTO tag2parent_tree (tag,parent,depth) SELECT t.tag, t.parent, t.depth FROM _tmp t ON DUPLICATE KEY UPDATE depth=LEAST(tag2parent_tree.depth, t.depth);
+        INSERT INTO _tmp SELECT t2pt.id, t2p.parent, t2pt.depth+1 FROM tag2parent_tree t2pt JOIN tag2parent t2p ON t2p.id=t2pt.parent ON DUPLICATE KEY UPDATE depth=LEAST(_tmp.depth, t2pt.depth+1);
+        INSERT INTO tag2parent_tree (id,parent,depth) SELECT t.id, t.parent, t.depth FROM _tmp t ON DUPLICATE KEY UPDATE depth=LEAST(tag2parent_tree.depth, t.depth);
     UNTIL ROW_COUNT() = 0
     END REPEAT;
 END $$
