ae6fece9bdd62957cf62e3bbde7a4bcd6082bc6e
NotCompsky <adammarkgray@gmail.com>
2020-07-18 15:07:57 +0100

    Feature		Allow compile-time pre-populating of the main tables. 		This is used to generate the static site hosted on GitHub Pages.

--- wangle-server/src/static/document.on-ready.js
+++ wangle-server/src/static/document.on-ready.js
@@ -49,6 +49,21 @@ function $$$on_document_ready(){
 	$$$add_key_intercepts();
 	
 	$$$document_getElementById('help-text').innerText = `
+Credits
+	The layout of this site's HTML is based on 'Responsive Social Platform' by Aysenur Turk: https://codepen.io/TurkAysenur/details/RwWKYMO
+	Almost all SVG icons are sourced or derived from tabler-icons by Paweł Kuna: https://github.com/tabler/tabler-icons
+		The exceptions are the two triangle icons by myself, and the search icon by Aysenur Turk
+	JavaScript libraries used:
+		JQuery
+		datetimepicker: https://github.com/xdan/datetimepicker
+			For the date-time picker dialog
+		PHP-Date-Formatter by Kartik Visweswaran: https://github.com/kartik-v/php-date-formatter
+			A dependency of datetimepicker
+		Select2: https://github.com/select2/select2
+			For fancier dropdown menus
+		MouseTrap: https://github.com/ccampbell/mousetrap
+			For intercepting key events
+
 Date Query
 	USAGE
 		[DATE]
@@ -66,4 +81,6 @@ Date Query
 
 Suppose you have a screenshot of a Twitter post. You can get the link to the original post - for instance, by using 'find-tweet' from https://github.com/NotCompsky/utils. You can then go to that file, click "Update src", and change the source to the Twitter post's URL. This will keep the screenshot as a 'backup' of the post.
 	`;
+	
+	!!!MACRO!!!ACTION_ON_DOCUMENT_LOAD
 }
