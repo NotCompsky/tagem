f026e888aa72727c35d252fac1c7829703d47c0d
NotCompsky <adammarkgray@gmail.com>
2020-07-20 00:14:09 +0100

    Tidy/Fix/UI

--- wangle-server/src/static/js/admin.js
+++ wangle-server/src/static/js/admin.js
@@ -1,16 +1,11 @@
 function $$$toggle_admin_dashboard(){
-	$$$hide_all_except(['admin-dashboard']);
+	if($$$logged_in())
+		$$$hide_all_except(['admin-dashboard']);
 }
 
 
 function $$$recursively_record_filesystem_dir_dialog(){
-	if($$$is_visible('record-fs-dir')){
-		$$$hide('record-fs-dir');
-		$$$hide('tagselect-files');
-	} else {
-		$$$unhide('record-fs-dir');
-		$$$unhide('tagselect-files');
-	}
+	$$$toggle('record-fs-dir');
 }
 
 function $$$recursively_record_filesystem_dir(){
@@ -20,8 +15,11 @@ function $$$recursively_record_filesystem_dir(){
 	const max_depth = $$$get_int_with_prompt("Maximum depth - 0 being 'no limit'");
 	if (max_depth === null)
 		return;
+	let tag_ids = $('#tagselect-record-fs-dir').val().join(",");
+	if (tag_ids === "")
+		tag_ids = "0";
 	$$$ajax_POST_data_w_text_response(
-		"!!!MACRO!!!SERVER_ROOT_URL/record-local-dir/"+max_depth+"/"+tag_ids,
+		"/record-local-dir/"+max_depth+"/"+tag_ids,
 		s,
 		function(){
 			$$$hide('record-fs-dir');
