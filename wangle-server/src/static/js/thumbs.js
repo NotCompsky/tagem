927b4237b7b58eea30e6573bd451f3cb2ebb888f
NotCompsky <adammarkgray@gmail.com>
2020-07-22 21:36:10 +0100

    Tidy

--- wangle-server/src/static/js/thumbs.js
+++ wangle-server/src/static/js/thumbs.js
@@ -1,10 +1,8 @@
 function $$$set_thumbnail_width(n){
 	$$$set_cookie("w", n, 3600);
-	$$$document_getElementById('css-thumb-sz').remove();
-	const x = $$$document.createElement('style');
-	x.id = 'css-thumb-sz';
-	x.innerHTML='.thumbnail img{max-width:'+n+'px; max-height:'+n+'px;}';
-	$$$document.head.appendChild(x);
+	const c = $$$document.styleSheets[0];
+	c.deleteRule(0),
+	c.insertRule('.thumbnail img{max-width:'+n+'px; max-height:'+n+'px;}', 0);
 }
 function $$$prompt_thumb_w(){
 	$$$set_thumbnail_width(parseInt($$$prompt("Width")));
