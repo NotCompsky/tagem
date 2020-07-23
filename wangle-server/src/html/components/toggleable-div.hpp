f9155a39ea5dbde85e40a42e0bfd209d986cad46
NotCompsky <adammarkgray@gmail.com>
2020-07-23 16:24:23 +0100

    Attempt

---
+++ wangle-server/src/html/components/toggleable-div.hpp
@@ -0,0 +1,6 @@
+"<a onclick=\"" MINIMISED_JS_DECL_toggle "('" TOGGLEABLE_DIV_ID "')\">Show " TOGGLEABLE_DIV_ID "</a>"
+"<div id=\"" TOGGLEABLE_DIV_ID "\" class=\"hidden help\"><p>"
+	#include HTML_INCLUDER(TOGGLEABLE_DIV_ID)
+"</p></div>"
+
+#undef TOGGLEABLE_DIV_ID
