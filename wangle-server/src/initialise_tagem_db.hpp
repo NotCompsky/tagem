f5e7690c7b054641c07db9af9331659393c100b1
NotCompsky <adammarkgray@gmail.com>
2020-07-28 16:40:01 +0100

    Fix		Create users first. Fix		Split on ';'

--- wangle-server/src/initialise_tagem_db.hpp
+++ wangle-server/src/initialise_tagem_db.hpp
@@ -6,15 +6,19 @@
 void initialise_tagem_db(MYSQL* mysql_obj){
 	constexpr static const char* const stmts =
 		#include "../../utils/src/init.sql"
-		#include "../../utils/src/init_data.sql"
-		#include "../../utils/src/triggers.sql"
-		#include "../../utils/src/procedures.sql"
 		#include "../../utils/src/init_user_invalid.sql"
 		#include "../../utils/src/init_user_guest.sql"
 		#include "../../utils/src/init_user_admin.sql"
+		#include "../../utils/src/init_data.sql"
+		#include "../../utils/src/triggers.sql"
+		#include "../../utils/src/procedures.sql"
 	;
 	
-	for (const char* itr = stmts;  *itr != 0;  ++itr)
-		if (unlikely(*itr == ';'))
-			compsky::mysql::exec_buffer(mysql_obj, stmts,  (uintptr_t)itr - (uintptr_t)stmts);
+	const char* last_stmt = stmts;
+	for (const char* itr = stmts;  *itr != 0;  ++itr){
+		if (unlikely(*itr == ';')){
+			compsky::mysql::exec_buffer(mysql_obj,  last_stmt,  (uintptr_t)itr - (uintptr_t)last_stmt);
+			last_stmt = itr + 1;
+		}
+	}
 }
