8101cc7fc5642148c882b2d50d4968c6d85a5983
NotCompsky <adammarkgray@gmail.com>
2020-10-04 13:08:45 +0100

    Revert "Failed/Attempt/Fix		Use RAII to avoid manual memory management."
    This reverts commit c61b5d128b731fb06e2521e9bd42595b9ba2b2a2.

--- wangle-server/src/python_stuff.cpp
+++ wangle-server/src/python_stuff.cpp
@@ -18,7 +18,6 @@ The absense of this copyright notices on some other files in this project does n
 #include <stdexcept>
 #include <mutex>
 #include <Python.h> // WARNING: Must be before any Qt includes, because slots is a macro name.
-#include <memory> // for std::unique_ptr
 
 
 #define PY_ASSERT_NOT_NULL(obj, msg) \
@@ -34,13 +33,6 @@ namespace python {
 // youtube_dl.YoutubeDL(options).download(args)
 // MODULE     INSTANTIATION      FUNCTION
 
-typedef std::shared_ptr<PyObject> PyObjectPtr;
-
-static
-PyObjectPtr make_py_obj_ptr(PyObject* const obj){
-	return PyObjectPtr(obj,  [](PyObject* o){ Py_DECREF(o); });
-}
-
 static PyObject* ytdl_obj;
 
 namespace tagem_module {
@@ -48,7 +40,7 @@ namespace tagem_module {
 	
 	static PyObject* modul;
 	
-	static PyObjectPtr file_path;
+	static PyObject* whyyyyyyyyyyyyyyyy;
 	
 	static
 	PyObject* to_stdout_fn(PyObject* const self,  PyObject* const args,  PyObject* const keyword_args){
@@ -58,14 +50,15 @@ namespace tagem_module {
 		// So as a workaround, static C-string is set, and mutex lock is needed in the calling function
 		// In reality, the download() function could take many minutes, so a mutex lock is completely infeasible. Hence we must *hope* that there won't be a data race.
 		
-		file_path = make_py_obj_ptr(PyTuple_GetItem(args, 0));
+		PyObject* const file_path = PyTuple_GetItem(args, 0);
 		if (unlikely(file_path == nullptr)){
 			Py_RETURN_NONE;
 		}
-		const char* const str = PyUnicode_AsUTF8(file_path.get());
+		const char* const str = PyUnicode_AsUTF8(file_path);
 		if (not os::is_local_file_or_dir(str))
 			Py_RETURN_NONE;
 		log("filepath == ", str);
+		whyyyyyyyyyyyyyyyy = file_path;
 		Py_RETURN_NONE;
 	}
 	
@@ -103,53 +96,65 @@ void init_ytdl(){
 	PY_ASSERT_NOT_NULL(ytdl_module, "Cannot import youtube_dl");
 	ytdl_obj = PyObject_GetAttrString(ytdl_module, "YoutubeDL");
 	PY_ASSERT_NOT_NULL(ytdl_obj, "Cannot find youtube_dl.YoutubeDL");
+	Py_INCREF(ytdl_module);
+	Py_INCREF(ytdl_obj);
 }
 
 bool ytdl(char* const out_fmt_as_input__resulting_fp_as_output,  const char* const url){
-	PyObjectPtr opts = make_py_obj_ptr(PyDict_New());
+	PyObject* const urls_ls = Py_BuildValue("[s]", url); // Or [item]?
+	
+	PyObject* const opts = PyDict_New();
 	PyDict_SetItem(
-		opts.get(),
+		opts,
 		PyUnicode_FromString("quiet"),
 		Py_True
 	);
 	PyDict_SetItem(
-		opts.get(),
+		opts,
 		PyUnicode_FromString("forcefilename"), // Forces it to 'print' the filename in to_stdout
 		Py_True
 	);
 	PyDict_SetItem(
-		opts.get(),
+		opts,
 		PyUnicode_FromString("outtmpl"),
 		PyUnicode_FromString(out_fmt_as_input__resulting_fp_as_output)
 	);
 	PyDict_SetItem(
-		opts.get(),
+		opts,
 		PyUnicode_FromString("format"),
 		PyUnicode_FromString(YTDL_FORMAT)
 	);
-	PyObjectPtr instantiation_args = make_py_obj_ptr(PyTuple_New(1));
-	PY_ASSERT_NOT_NULL(instantiation_args.get(), "Cannot instantiate instantiation args");
-	PyTuple_SetItem(instantiation_args.get(), 0, opts.get());
-	PyObjectPtr ytdl_instantiation = make_py_obj_ptr(PyObject_CallObject(ytdl_obj, instantiation_args.get())); // Error here
-	PY_ASSERT_NOT_NULL(ytdl_instantiation.get(), "Cannot instantiate YoutubeDL object");
+	PyObject* const instantiation_args = PyTuple_New(1);
+	PY_ASSERT_NOT_NULL(instantiation_args, "Cannot instantiate instantiation args");
+	PyTuple_SetItem(instantiation_args, 0, opts);
+	PyObject* const ytdl_instantiation = PyObject_CallObject(ytdl_obj, instantiation_args); // Error here
+	PY_ASSERT_NOT_NULL(ytdl_instantiation, "Cannot instantiate YoutubeDL object");
+	PyObject* const ytdl_fn = PyObject_GetAttrString(ytdl_instantiation, "download");
 	
 	// Override to_stdout, so that the file path is written to a buffer instead of stdout
-	PyObject_SetAttrString(ytdl_instantiation.get(), "to_stdout", tagem_module::to_stdout);
+	PyObject_SetAttrString(ytdl_instantiation, "to_stdout", tagem_module::to_stdout);
 	/*PyObject_SetAttrString(tagem_module::modul, "ytdl_instantiation", ytdl_instantiation);
 	PyRun_String("tagem.ytdl_instantiation.to_stdout = tagem.to_stdout", Py_single_input, nullptr, nullptr);
 	// The C API call acts differently - the first argument is NOT a pointer to the 'self' object, but remains a pointer to the module object*/
 	
-	PyObjectPtr fn_args = make_py_obj_ptr(PyTuple_New(1));
-	PyObjectPtr urls_ls = make_py_obj_ptr(Py_BuildValue("[s]", url)); // Or [item]?
-	PyTuple_SetItem(fn_args.get(), 0, urls_ls.get());
-	PyObjectPtr ytdl_fn = make_py_obj_ptr(PyObject_GetAttrString(ytdl_instantiation.get(), "download"));
-	PY_ASSERT_NOT_NULL(ytdl_fn.get(), "ytdl_fn is NULL");
-	PyObjectPtr result = make_py_obj_ptr(PyObject_CallObject(ytdl_fn.get(), fn_args.get()));
+	PyObject* const fn_args = PyTuple_New(1);
+	PyTuple_SetItem(fn_args, 0, urls_ls);
+	const PyObject* const result = PyObject_CallObject(ytdl_fn, fn_args);
 	
+	PyObject* const file_path = tagem_module::whyyyyyyyyyyyyyyyy;
 	Py_ssize_t sz;
-	const char* const _file_path = PyUnicode_AsUTF8AndSize(tagem_module::file_path.get(), &sz);
+	const char* const _file_path = PyUnicode_AsUTF8AndSize(file_path, &sz);
 	memcpy(out_fmt_as_input__resulting_fp_as_output, _file_path, sz);
 	out_fmt_as_input__resulting_fp_as_output[sz] = 0;
+	
+	Py_DECREF(file_path);
+	Py_DECREF(result);
+	Py_DECREF(fn_args);
+	Py_DECREF(ytdl_fn);
+	Py_DECREF(ytdl_instantiation);
+	Py_DECREF(instantiation_args);
+	Py_DECREF(opts);
+	Py_DECREF(urls_ls);
 	
 	return false;
 }
