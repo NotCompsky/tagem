0bfe417802465015999ab6f474e7888eb7ecb42f
NotCompsky <adammarkgray@gmail.com>
2020-10-13 14:45:30 +0100

    Fix		Force libcompsky to include std::string_view

--- wangle-server/src/server.cpp
+++ wangle-server/src/server.cpp
@@ -11,6 +11,7 @@ This copyright notice should be included in any copy or substantial copy of the 
 The absense of this copyright notices on some other files in this project does not indicate that those files do not also fall under this license, unless they have a different license written at the top of the file.
 */
 #define DEBUG
+#define COMPSKY_STD_STRING_VIEW
 
 #define CACHE_CONTROL_HEADER "Cache-Control: max-age=" MAX_CACHE_AGE "\n"
 
