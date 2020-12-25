4e20bea44466470ab167b281580b6f6f2d265e48
NotCompsky <adammarkgray@gmail.com>
2020-12-25 15:42:35 +0000

    Tidy Fix		Avoid use of static to make thread-safe

--- wangle-server/src/thumbnailer.hpp
+++ wangle-server/src/thumbnailer.hpp
@@ -25,11 +25,11 @@ void generate_thumbnail(const char* const input_path,  const char* const output_
 	constexpr bool smart_frame_selection = true;
 	constexpr int thumbnail_size = 256;
 	
-	static FilmStripFilter filmstrip_filter;
+	FilmStripFilter filmstrip_filter;
 	
 	constexpr ThumbnailerImageType imageType = ThumbnailerImageType::Png;
 	
-	static VideoThumbnailer video_thumbnailer(0, workaround_issues, maintain_aspect_ratio, quality, smart_frame_selection);
+	VideoThumbnailer video_thumbnailer(0, workaround_issues, maintain_aspect_ratio, quality, smart_frame_selection);
 	video_thumbnailer.setThumbnailSize(thumbnail_size);
 	video_thumbnailer.addFilter(&filmstrip_filter);
 	
